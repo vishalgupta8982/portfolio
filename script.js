988f6d634f540eeb1bcfa387fe4ceee9dd00c768
vishalgupta8982 <108549206+vishalgupta8982@users.noreply.github.com>
2024-04-28 23:55:03 +0530

    second

--- script.js
+++ script.js
@@ -35,17 +35,17 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web Developer",  "Web Designer" ],
+        strings: [ "Mobile Application Developer","Full Stack Web Devloper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Web Developer", "Web Designer", ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
+      strings: ["Mobile Application Developer", "Full Stack Web Devloper"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
     });
 
     // owl carousel script
